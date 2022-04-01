ed0794840eb82b7cfa51fa79ac2bd2a3563a5aaf
KadeWilliams <kadewilliams0@gmail.com>
2022-03-31 19:00:11 -0500

    added card features and dynamic card additions

--- book.js
+++ book.js
@@ -7,6 +7,8 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+const gridOuter = document.querySelector('.grid-outer')
+
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
@@ -15,26 +17,79 @@ const read = document.querySelector('#read');
 const subBtn = document.querySelector('.submit')
 const printBtn = document.querySelector('.print')
 
+
+
+
 function addBookToLibrary() {
-    let newBook = new Book(title.value, author.value, pages.value, read.value)
-    myLibrary.push(newBook)
-    clearInputs();
+    let newBook;
+    if (title.value !== '' && author.value !== '' && pages.value !== '') {
+        newBook = new Book(title.value, author.value, pages.value, read.checked)
+        // myLibrary.push(newBook)
+        clearInputs();
+    }
+    const newCard = document.createElement('div')
+    newCard.classList.add('card')
+
+    const newTitle = document.createElement('p');
+    newTitle.classList.add('title');
+    newTitle.innerText = `"${newBook.title}"`;
+
+    const newAuthor = document.createElement('p');
+    newAuthor.classList.add('author');
+    newAuthor.innerText = newBook.author;
+
+    const newPages = document.createElement('p');
+    newPages.classList.add('pages');
+    newPages.innerText = `${newBook.pages} pages`
+
+
+    const newReadBox = document.createElement('button');
+    newReadBox.classList.add('readBox');
+    if (newBook.read == true) {
+        newReadBox.innerText = 'Read';
+        newReadBox.classList.add('beenRead');
+    } else {
+        newReadBox.innerText = 'Not read';
+        newReadBox.classList.add('notRead');
+    }
+    newReadBox.addEventListener('click', () => {
+        if (newReadBox.classList.contains('beenRead')) {
+            newReadBox.innerText = 'Not read';
+            newReadBox.classList.remove('beenRead');
+            newReadBox.classList.add('notRead');
+        } else {
+            newReadBox.innerText = 'Read';
+            newReadBox.classList.remove('notRead');
+            newReadBox.classList.add('beenRead')
+        }
+    })
+
+
+    const newRemove = document.createElement('button');
+    newRemove.classList.add('remove-btn');
+    newRemove.innerText = 'Remove';
+
+    newCard.appendChild(newTitle);
+    newCard.appendChild(newAuthor);
+    newCard.appendChild(newPages)
+    newCard.appendChild(newReadBox);
+    newCard.appendChild(newRemove);
+    gridOuter.appendChild(newCard);
+
+    const lastCard = gridOuter.lastChild;
+    lastCard.lastChild.addEventListener('click', () => {
+        gridOuter.removeChild(gridOuter.lastChild)
+    })
 }
+
 function clearInputs() {
     title.value = '';
     author.value = '';
     pages.value = '';
-    read.value = '';
+    read.checked = false;
 }
 
 subBtn.addEventListener('click', addBookToLibrary)
 
-printBtn.addEventListener('click', () => {
-    console.log(myLibrary)
-    for (let book of myLibrary) {
-        console.log(book)
-    }
-}
-)
 
 
